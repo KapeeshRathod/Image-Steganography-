2fa690b2af74bb5660cb601d0db8a4c853200cda
KapeeshRathod <165982445+KapeeshRathod@users.noreply.github.com>
2025-11-30 14:02:37 +0530

    Add files via upload

---
+++ src/datasteganography/DataSteganography.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datasteganography;
+
+/**
+ *
+ * @author welcome
+ */
+public class DataSteganography {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        LoginFrame lf=new LoginFrame();
+        lf.setVisible(true);
+        lf.setSize(950,600);
+       lf.setLocation(500, 100);
+    }
+    
+}
